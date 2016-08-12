e1dc4edaec6a566ecc89391c260a26ff8272fd47
vincent.su <gnux123@gmail.com>
2016-08-12 14:45:05 +0800

    first version.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# gulp-clear-enviorment
-clear gulp develop enviorment
+# gulp-clear-enviorment v1.0
+
+---
+
+## 安裝方式
+
+- 初次使用方式：
+
+    * 安裝全域 **gulp**及**node-sass**
+
+        ```
+        $> npm install -g gulp
+
+
+        $> npm install -g node-sass
+
+        ```
+
+- 安裝套件方式：
+
+    ```
+    $> npm install
+    ```
+
+---
+
+## 執行gulp開發任務：
+
+    ```
+    //執行local server 開發環境
+    $> gulp server
+
+    //執行build動作，產生dist包
+    $> gulp build
+    ```
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var gulp = require('gulp');
+var del = require('del');
+var sass = require('gulp-sass');
+var cssminify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var include = require('gulp-html-tag-include');
+
+//server load
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+//config
+var config = {
+	app: "app",
+	temp: "tmp",
+	dest: "dest",
+	jsfolder: "/scripts"
+};
+
+//server task
+gulp.task('browserSync', ['clean'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./tmp"
+        }
+    });
+
+	return gulp.watch([
+				config.app + '/{,*/}*.html',
+				config.app + '/scripts/*.js',
+				config.app + '/scss/{*,*/,*/*/,*/*/*/}*.scss'
+			], ['html-include', 'sass', 'copy']).on("change", reload);
+});
+
+gulp.task('sass', function(){
+	return gulp.src(config.app + '/scss/{*,*/,*/*/,*/*/*/}*.scss')
+			   .pipe(sass({
+	                    includePaths: [
+							'./node_modules/font-awesome',
+							'./node_modules/compass-mixins/lib',
+							'./node_modules/susy/sass',
+							'./node_modules/animatewithsass/'
+						],
+						outputStyle: 'expanded',
+						precision: 10,
+						errLogToConsole: true
+	                }).on('error', sass.logError)
+				)
+			   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+			   .pipe(gulp.dest(config.temp + '/css'));
+});
+
+//html template include task
+gulp.task('html-include', function() {
+	return gulp.src(config.app+'/*.html', {base: config.app})
+		.pipe(include())
+		.pipe(gulp.dest(config.temp));
+});
+
+//copy task
+gulp.task('copy', function(){
+	return gulp.src([
+					config.app + '/scripts/*.js',
+					config.app + '/fonts/*.*',
+					config.app + '/images/{*,*/}*.*',
+					config.app + '/Themes/{*,*/,*/*/,*/*/*/}*.*'
+				], { base: config.app })
+			   .pipe(gulp.dest(config.temp));
+});
+
+
+//dist tasks
+gulp.task('dist:html-include', function() {
+	return gulp.src(config.app+'/*.html', {base: config.app})
+		.pipe(include())
+		.pipe(gulp.dest(config.dest));
+});
+
+gulp.task('dist:css', function(){
+	return gulp.src(config.temp + '/css/*.css', { base: config.temp })
+			   .pipe(cssminify())
+			   .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('dist:copy', function(){
+	return gulp.src([
+					config.app + '/scripts/*.js',
+					config.app + '/fonts/*.*',
+					config.app + '/images/{*,*/}*.*'
+				], { base: config.app })
+			   .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('dist:uglify', function(){
+	return gulp.src([
+		config.dest + config.jsfolder + '/*.js'
+	], { base: config.dest })
+	.pipe(uglify())
+	.pipe(gulp.dest(config.dest));
+});
+
+//clean task
+gulp.task('clean', function(cb){
+    return del(['tmp', 'dest'], {force: true, read: false}, cb);
+});
+
+gulp.task('server', ['browserSync'], function(){
+	gulp.start(['clean', 'sass', 'copy', 'html-include']);
+});
+
+gulp.task('build', ['dist:html-include', 'dist:copy', 'dist:uglify', 'dist:css']);
