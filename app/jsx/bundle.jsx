5f9496fff9fd05e9fd398d0930e4ad9d5e5228f4
vincent.su <gnux123@gmail.com>
2017-07-18 13:21:32 +0800

    增加es6寫法

--- app/jsx/bundle.jsx
+++ app/jsx/bundle.jsx
@@ -1,13 +1,38 @@
-var HelloWorld = React.createClass({
-    getInitialState: function(){
-        return {
-            text: "Hello React World!!!"
-        }
-    },
 
-    render: function(){
-        return(<h2>{this.state.text}</h2>)
+/**
+ * es2015寫法
+** */
+// var HelloWorld = React.createClass({
+//     getInitialState: function(){
+//         return {
+//             text: "Hello React World!!!"
+//         }
+//     },
+
+//     render: function(){
+//         return(<h2>{this.state.text}</h2>)
+//     }
+// });
+
+/**
+ * es6寫法
+ */
+
+class HelloWorld extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            text: "Hello React World!!!"
+        };
     }
-});
+
+    render() {
+        return(<h2>
+            {this.state.text}
+            <MyShowExtend />
+        </h2>);
+    }
+}
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
