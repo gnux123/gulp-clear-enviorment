cd0f0ab93c3018b8ba647473f601f170b2474d8d
vincent.su <gnux123@gmail.com>
2017-07-20 17:29:44 +0800

    mobx測試範例

--- app/jsx/bundle.jsx
+++ app/jsx/bundle.jsx
@@ -1,3 +1,57 @@
+const { observable, computed, action, useStrict } = mobx;
+const { observer } = mobxReact;
+
+mobx.useStrict(true);
+
+/**
+ * mobx test
+ */
+class Store {
+    @observable todos = [
+        {
+        title: "todo标题",
+        done: false
+        },
+        {
+        title: "todo标题2",
+        done: false
+        }
+    ];
+
+    @action changeData(index, title) {
+        console.log(1);
+        this.todos[index].title = title;
+    }
+}
+
+@observer
+class TodoBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    getData(e){
+        console.log(this.props.store);
+        this.props.store.changeData(0,"我要成為海賊王");
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.store.todos.map(todo => <li>{todo.title}</li>)}
+                </ul>
+                <div>
+                    <a onClick={this.getData.bind(this)}>按我拉</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+const store = new Store();
+
 
 /**
  * es2015寫法
@@ -11,7 +65,7 @@ var HelloWorld = React.createClass({
 
     componentWillMount: function(){
         this.setState({
-            text: "hello my vincent"
+            text: "hello my vincent2"
         });
     },
 
@@ -23,27 +77,59 @@ var HelloWorld = React.createClass({
 /**
  * es6寫法
  */
+const logoGo = (add, text) => {
+    let ShowText = add + text;
+    return ShowText;
+};
+
+class AboutMe extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            aboutText: ""
+        };
+    }
+
+    componentWillMount() {
+        this.setState({
+            aboutText: (!!this.props.title)?this.props.title:this.state.aboutText
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <br/>
+                <p>{this.state.aboutText}</p>
+                <span>{logoGo("myName:","vincent")}</span>
+            </div>
+        );
+    }
+}
 
 export default class HelloWorldES6 extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-            text: "Hello React ES6 World!!!"
+            text: "Hello React ES6 World!!!",
+            aboutTitle: "vincent.sue Show Time"
         };
     }
 
     componentWillMount(){
-        this.setState({text: "hello my ES6"});
+        this.setState({text: "hello my ES61"});
     }
 
     render() {
-        return(<h3>
+        return(<h3 className="es6_title">
             {this.state.text}
+            <AboutMe title={this.state.aboutTitle} />
         </h3>);
     }
 }
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
 
-ReactDOM.render(<HelloWorldES6 />, document.getElementById('es6root'));
+ReactDOM.render(<TodoBox store={store} />, document.getElementById('es6root'));
