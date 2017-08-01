cb5f10500b0e9889b4a4609a095860a6a842fac0
vincent.su <gnux123@gmail.com>
2017-08-01 10:43:55 +0800

    mobx嚴格模式修正

--- app/jsx/bundle.jsx
+++ app/jsx/bundle.jsx
@@ -1,36 +1,36 @@
 const { observable, computed, action, useStrict, autorun } = mobx;
 const { observer } = mobxReact;
 
+useStrict(true);
+
 /**
  * mobx test
  */
 class Store {
-    @observable todos = [
-        {
-        title: "todo标题",
-        done: false
-        },
-        {
-        title: "todo标题2",
-        done: false
-        }
-    ];
-
     @observable parkData = [];
 
 
     constructor() {
         autorun(() => {
-            this.changeData();
+            // this.changeData();
             // console.log(this.parkData);
         });
     }
 
 
     @action changeData(){
-        $.getJSON('https://gist.githubusercontent.com/gnux123/24c17078ae6b1ccf6719c7b1c55ab039/raw/6f6ed57784a5eaae955ec38696861e1bbc6615e6/kh.json').then(function(result){
+
+        /**
+         * 結束觸發的callback function, 被@action給包住了,
+         * 因為@action無法影響當前function調用的ajax，而這個callback毫無疑問是一個ajax，
+         * 所以必須再用一個action來包住它，這樣程序才不會報錯
+         */
+
+        $.getJSON('https://gist.githubusercontent.com/gnux123/24c17078ae6b1ccf6719c7b1c55ab039/raw/6f6ed57784a5eaae955ec38696861e1bbc6615e6/kh.json')
+         .then(action("Get Kaohsiung Park Json Data.", function (result) {
+            //do something
             this.parkData = result;
-        }.bind(this));
+         }.bind(this)));
     }
 
     //計算parkData
@@ -40,8 +40,6 @@ class Store {
             var _parkName = this.parkData[i].Name;
             _parkNameArr.push(_parkName);
         }
-
-        console.log(_parkNameArr);
         return _parkNameArr;
     }
 
