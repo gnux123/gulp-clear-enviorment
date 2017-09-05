314d15a884ae6e8c00e59b8c2eac42460fa729b6
vincent.su <gnux123@gmail.com>
2017-09-05 17:14:08 +0800

    調整webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 process.noDeprecation = true;
+const webpack = require('webpack');
 
 module.exports = {
     devtool: "source-map",
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "React": "React",
+        "ReactDOM": "ReactDOM"
+    },
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -13,9 +20,19 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2016', 'react'],
-                    plugins: ['transform-decorators-legacy', 'transform-class-properties']
+                    plugins: [
+                        'transform-decorators-legacy',
+                        'transform-class-properties'
+                    ]
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 };
